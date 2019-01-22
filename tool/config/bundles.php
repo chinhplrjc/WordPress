3698c4a6d836c8c0a36d4cffb1a75eb04308a0d0
root <chinhpl@wealth-park.com>
2019-01-22 06:07:44 +0000

    create web bundle

--- tool/config/bundles.php
+++ tool/config/bundles.php
@@ -3,4 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle::class => ['all' => true],
+    Sensio\Bundle\DistributionBundle\SensioDistributionBundle::class => ['all' => true],
 ];
